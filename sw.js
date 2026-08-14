13db09c76c50dd1eba1de815823bc44badcce023
gurkenfurzi <muelliaccc@gmail.com>
2026-08-14 08:09:25 +0200

    Add files via upload

--- sw.js
+++
@@ -1,1 +0,0 @@
-const V="schoolbloom-v83";self.addEventListener("install",e=>self.skipWaiting());self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener("fetch",e=>{if(e.request.mode==="navigate")e.respondWith(fetch(e.request,{cache:"no-store"}).catch(()=>caches.match(e.request)));});
