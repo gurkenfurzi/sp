591a5110465aabe647ab3531965f9ca505a2dd62
gurkenfurzi <muelliaccc@gmail.com>
2026-09-06 23:45:17 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,2 +1,3 @@
 self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',event=>{event.waitUntil((async()=>{try{for(const k of await caches.keys()){if(/studia|schoolbloom/i.test(k))await caches.delete(k)}}catch(_){}try{await self.registration.unregister()}catch(_){}try{for(const c of await self.clients.matchAll({type:'window'})){c.navigate(c.url)}}catch(_){}})())});
+self.addEventListener('activate',event=>event.waitUntil((async()=>{try{for(const key of await caches.keys()){if(/studia|schoolbloom/i.test(key))await caches.delete(key)}}catch(_){}await self.clients.claim()})()));
+self.addEventListener('fetch',()=>{});
