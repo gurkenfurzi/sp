2537a1d5fb5a5252c23288f5a5dbb6e1187ccbe6
gurkenfurzi <muelliaccc@gmail.com>
2026-09-11 23:33:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,3 @@
-// Studia v186 desktop interaction build
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',event=>event.waitUntil((async()=>{try{for(const key of await caches.keys()){if(/studia|schoolbloom/i.test(key))await caches.delete(key)}}catch(_){}await self.clients.claim()})()));
 self.addEventListener('fetch',()=>{});
